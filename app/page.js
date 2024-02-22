c4a0fc8188a77fc8d5f7dd66c03e06c723352466
samir <febsec14@gmail.com>
2024-02-22 17:06:44 +0545

    second commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
-
+import Image from 'next/image';
 export default function Home() {
   return (
-	  <main>
-	  <h1>Rosmita page</h1>
+    <main className='flex flex-col items-center '>
+      <h1 className='text-4xl font-bold'>Rosmita page</h1>
+      <Image
+        src="/images/1.jpg"
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      />
     </main>
   );
 }
